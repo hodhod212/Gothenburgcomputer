e26645a07a317fa18e3ef35443975040c71ceac0
hodhod222 <aliezadkhaha212@gmail.com>
2022-03-07 10:08:56 +0100

    Creating netflix clone by using nextjs

---
+++ components/banner/banner.js
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import styles from "./banner.module.css";
+const Banner = ({ title, imgUrl, subTitle }) => {
+  const handleOnPlay = () => {
+    console.log("handleOnPlay");
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.leftWrapper}>
+        <div className={styles.left}>
+          <div className={styles.nseriesWrapper}>
+            <p className={styles.firstLetter}>N</p>
+            <p className={styles.series}>S E R I E S</p>
+          </div>
+          <h3 className={styles.title}>{title}</h3>
+          <h3 className={styles.subTitle}>{subTitle}</h3>
+          <div className={styles.playBtnWrapper}>
+            <button className={styles.btnWithIcon} onClick={handleOnPlay}>
+              <Image
+                src="/static/play_arrow.svg"
+                alt="play icon"
+                width={32}
+                height={32}
+              />
+              <span className={styles.playText}>Play</span>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div
+        style={{
+          backgroundImage: `url(${imgUrl})`,
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          backgroundSize: "cover",
+          backgroundPosition: "50% 50%",
+        }}
+        className={styles.bannerImg}
+      ></div>
+    </div>
+  );
+};
+
+export default Banner;
